cb1046d1572f3c05646224395e0b8dd2be0428c8
calebearcilio <calebearcilio@gmail.com>
2024-11-14 22:15:38 -0300

    adicionado verificação de confirmação de senha.

--- acesso/cadastro/cadastro.php
+++ acesso/cadastro/cadastro.php
@@ -4,46 +4,56 @@ include_once "../../config.inc.php";
 $nome_usuario = "";
 $email_usuario = "";
 $senha_usuario = "";
+$confirmar_senha_usuario = "";
 $cadastrado = 0;
 
+
 //verifica se o usuario enviou os dados
-if (($_POST)) {
-    $nome_usuario = $_POST['nome_usuario'];
-    $email_usuario = $_POST['email_usuario'];
-    $senha_usuario = $_POST['senha_usuario'];
-}
-else{
-    //retorna para a pagina de cadastro com a mensagem de erro
+if (!$_POST) {
+    //retorna para a pagina de cadastro caso falhe ao enviar os dados
     header("Location: ../../?pg=acesso/form-cadastrar&cadastrado=0");
     mysqli_close($conexao);
     exit();
-}   
+}
+
+$nome_usuario = $_POST['nome_usuario'];
+$email_usuario = $_POST['email_usuario'];
+$senha_usuario = $_POST['senha_usuario'];
+$confirmar_senha_usuario = $_POST['confirmar_senha_usuario'];
 
 //verifica se o usuario ja existe
 $sql_verificacao = "SELECT * FROM usuarios WHERE nome = '$nome_usuario' OR email = '$email_usuario'";
 $query_verificacao = mysqli_query($conexao, $sql_verificacao);
 
 if (mysqli_num_rows($query_verificacao) > 0) {
-    //retorna para a pagina de cadastro com a mensagem de erro
+    //retorna para a pagina de cadastro caso o usuario ja exista
     header("Location: ../../?pg=acesso/form-cadastrar&cadastrado=2");
     mysqli_close($conexao);
     exit();
-}else{
-    //insere o usuario no banco de dados
-    $sql = "INSERT INTO usuarios (nome,email,senha) VALUES ('$nome_usuario','$email_usuario','$senha_usuario')";
-    $query = mysqli_query($conexao, $sql);
-
-    if ($query) {
-        //retorna para a pagina de cadastro com a mensagem de sucesso
-        header("Location: ../../?pg=acesso/form-cadastrar&cadastrado=1");
-        mysqli_close($conexao);
-        exit();
-    }else{
-        //retorna para a pagina de cadastro com a mensagem de erro
-        header("Location: ../../?pg=acesso/form-cadastrar&cadastrado=0");
-        mysqli_close($conexao);
-        exit();
-    }
 }
+
+if($senha_usuario != $confirmar_senha_usuario){
+    //retorna para a pagina de cadastro caso as senhas não correspondem
+    header("Location: ../../?pg=acesso/form-cadastrar&cadastrado=3");
+    mysqli_close($conexao);
+    exit();
+}
+
+//insere o usuario no banco de dados
+$sql = "INSERT INTO usuarios (nome,email,senha) VALUES ('$nome_usuario','$email_usuario','$senha_usuario')";
+$query = mysqli_query($conexao, $sql);
+
+if ($query) {
+    //retorna para a pagina de cadastro com a mensagem de sucesso
+    header("Location: ../../?pg=acesso/form-cadastrar&cadastrado=1");
+    mysqli_close($conexao);
+    exit();
+}else{
+    //retorna para a pagina de cadastro caso falhe ao cadastrar
+    header("Location: ../../?pg=acesso/form-cadastrar&cadastrado=0");
+    mysqli_close($conexao);
+    exit();
+}
+
 mysqli_close($conexao);
 ?>
--- acesso/form-cadastrar.php
+++ acesso/form-cadastrar.php
@@ -3,16 +3,17 @@
     <?php 
     if (isset($_GET['cadastrado'])) {
         $cadastrado = $_GET['cadastrado'];
-        switch($cadastrado) {
-            case '0':
-                echo "<h2 style='color: red;'>FALHA AO CADASTRAR</h2>";
-                break;
-            case '1':
-                echo "<h2 style='color: green;'>Usuário cadastrado com sucesso!</h2>";
-                break;
-            case '2':
-                echo "<h2 style='color: orange;'>Usuário ou email já existe!</h2>";
-                break;
+        if($cadastrado == '0') {
+            echo "<h2 style='color: red;'>FALHA AO CADASTRAR</h2>";
+        }
+        else if($cadastrado == '1') {
+            echo "<h2 style='color: green;'>Usuário cadastrado com sucesso!</h2>";
+        }
+        else if($cadastrado == '2') {
+            echo "<h2 style='color: orange;'>Usuário ou email já existe!</h2>";
+        }
+        else if($cadastrado == '3') {
+            echo "<h2 style='color: red;'>As senhas não correspondem!</h2>";
         }
     }
     ?>
@@ -20,6 +21,7 @@
         <input type="text" name="nome_usuario" id="nome_usuario" maxlength="50"  placeholder="Nome" required>
         <input type="email" name="email_usuario" id="email_usuario" maxlength="50" placeholder="Email" required>
         <input type="password" name="senha_usuario" id="senha_usuario" maxlength="50" placeholder="Senha" required>
+        <input type="password" name="confirmar_senha_usuario" id="confirmar_senha_usuario" maxlength="50" placeholder="Confirmar Senha" required>   
         <button type="submit">Cadastrar</button>
     </form>
 </div> 
