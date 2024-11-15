de24bf84900cf1ec7b473bbfa7a23037d2b7ef9b
Lucas Leandro <lucasfariasleandro@gmail.com>
2024-11-15 13:44:44 -0300

    Ajuste do botão para a página principal

--- components/menu/menu.php
+++ components/menu/menu.php
@@ -1,6 +1,6 @@
 <nav class="menu-navegacao">
     <ul>
-        <li><a href="#">Menu</a></li>
+        <li><a href="?pg=components/conteudo/conteudo">Menu</a></li>
         <li><a href="#">Perfil</a></li>
         <li><a href="#">Amizades</a></li>
         <li><a href="#">Fale Conosco</a></li>
