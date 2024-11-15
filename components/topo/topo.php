75a5063250a1a4f5fdd2ecc4e362b92f1eea57e2
Lucas Leandro <lucasfariasleandro@gmail.com>
2024-11-15 13:42:59 -0300

    ajuste da vizualização dos botões

--- components/topo/topo.php
+++ components/topo/topo.php
@@ -3,7 +3,7 @@
         <h1>CineBookly</h1>
     </div>
     <div class="botoes">
-        <button class="botao-login"><a href="#">Login</a></button>
-        <button class="botao-cadastro"><a href="?pg=acesso/form-cadastrar">Cadastro</a></button>
+        <button class="botao-login"><a href="?pg=acesso/login/form-login">Login</a></button>
+        <button class="botao-cadastro"><a href="?pg=acesso/cadastro/form-cadastrar">Cadastro</a></button>
     </div>
 </div>
