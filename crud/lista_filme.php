80c07ed451ae9dd25d578409dd84cd5f83f9b3e9
Ester Marreiro <estermarreiro@outlook.com>
2024-11-18 14:09:00 -0300

    adicao de uma condicional para que apenas usuarios logados consigam editar ou excluir seus proprios posts

--- crud/lista_filme.php
+++ crud/lista_filme.php
@@ -1,11 +1,15 @@
-<h2>Filmes</h2>
-
-<p><a href="?pg=form_cria_post">Criar Novo Post</a></p>
+<h1>Filmes</h1>
 
 <?php
 
-
+    session_start();
     include_once("../config.inc.php");
+
+    if (isset($_SESSION['id_usuario'])) {
+        echo '<h2><a href="form_cria_post.php?tipo=filme">Criar Novo Post</a></h2>'; //erro no envio do formulario a partir daqui. problema de referencia no form??
+    } else {
+        echo '<h2><a href="../acesso/login/form-login.php">Criar Novo Post</a></h2>';
+    }
 
     $sql = mysqli_query($conexao,"SELECT * FROM posts WHERE tipo = 'filme'");
 
@@ -14,9 +18,11 @@
         echo "Comentário: $tabela[comentario] <br>";
         echo "Avaliação:  $tabela[avaliacao]  <br>";
 
-        echo "<a href=?pg=exclui_post&id=$tabela[id]><b>[X] Excluir</b></a>  |";
-        echo "<a href=?pg=form_edita_post&id=$tabela[id]><b>[V] Editar</b></a> <br>";
-        echo "<hr>";
+        // as opcoes de exluir e editar so estarao visiveis para os usuarios logados e donos dos posts
+        if (isset($_SESSION['id_usuario']) && $_SESSION['id_usuario'] == $tabela['usuario_id']) {
+            echo "<a href='exclui_post.php?id=$tabela[id]&tipo=filme'><b>[X] Excluir</b></a> | ";
+            echo "<a href='form_edita_post.php?id=$tabela[id]&tipo=filme'><b>[V] Editar</b></a> <br>";
+        } echo "<hr>";
     }
 
     if(!$sql){
--- crud/lista_livro.php
+++ crud/lista_livro.php
@@ -1,11 +1,15 @@
-<h2>Livros</h2>
-
-<p><a href="?pg=form_cria_post">Criar Novo Post</a></p>
+<h1>Livros</h1>
 
 <?php
 
-
+    session_start();
     include_once("../config.inc.php");
+
+    if (isset($_SESSION['id_usuario'])) {
+        echo '<h2><a href="form_cria_post.php">Criar Novo Post</a></h2>';
+    } else {
+        echo '<h2><a href="../acesso/login/form-login.php">Criar Novo Post</a></h2>';
+    }
 
     $sql = mysqli_query($conexao,"SELECT * FROM posts WHERE tipo = 'livro'");
 
@@ -14,9 +18,11 @@
         echo "Comentário: $tabela[comentario] <br>";
         echo "Avaliação:  $tabela[avaliacao]  <br>";
 
-        echo "<a href=?pg=exclui_post&id=$tabela[id]><b>[X] Excluir</b></a>  |";
-        echo "<a href=?pg=form_edita_post&id=$tabela[id]><b>[V] Editar</b></a> <br>";
-        echo "<hr>";
+        // as opcoes de exluir e editar so estarao visiveis para os usuarios logados e donos dos posts
+        if (isset($_SESSION['id_usuario']) && $_SESSION['id_usuario'] == $tabela['usuario_id']) {
+            echo "<a href='exclui_post.php?id=$tabela[id]&tipo=livro'><b>[X] Excluir</b></a> | ";
+            echo "<a href='form_edita_post.php?id=$tabela[id]&tipo=livro'><b>[V] Editar</b></a> <br>";
+        } echo "<hr>";
     }
 
     if(!$sql){
