d00b9bed0389e98235cad75adca00b869170cfb1
Ester Marreiro <estermarreiro@outlook.com>
2024-11-18 13:37:57 -0300

    Adicao de verificacao de login e redirecionamento apos postagem; mais alteracao no campo de id do usuario
    o id do usuario nao vai mais ser recebido pelo banco, mas pela sessao

--- crud/exclui_post.php
+++ crud/exclui_post.php
@@ -2,14 +2,31 @@
 
     include_once("../config.inc.php");
 
+    session_start();
+    if (!isset($_SESSION['id_usuario'])) {  //so exclui se tiver logado
+        die("Erro: Usuário não está logado.");
+    }
+
     $id = $_REQUEST['id'];
+    $tipo =       $_REQUEST['tipo'];
 
-    $sql = mysqli_query($conexao,"DELETE FROM posts WHERE id = $id");
+    $sql = mysqli_query($conexao,"DELETE FROM posts WHERE id = $id AND usuario_id = {$_SESSION['id_usuario']}");
 
-    if($sql){
+    if ($sql) {
         echo "<h2>Post Excluido</h2>";
+    
+        if ($tipo == 'filme') {     //as variaveis de tipo vao ser fornecidas pela url
+            header("Location: lista_filme.php");
+            exit;
+        }else if ($tipo == 'serie'){
+            header("Location: lista_serie.php");
+            exit;
+        }else if ($tipo == 'livro'){
+            header("Location: lista_livro.php");
+            exit;
+        }
     } else {
-        echo "<h2>Falha na exclusão</h2>";
+        echo "<h2>Erro ao Excluir: " . mysqli_error($conexao) . "</h2>";
     }
 
     mysqli_close($conexao);
--- crud/form_edita_post.php
+++ crud/form_edita_post.php
@@ -2,6 +2,11 @@
 
     include_once("../config.inc.php");
 
+    session_start();
+    if (!isset($_SESSION['id_usuario'])) {  //so edita se tiver logado
+        die("Erro: Usuário não está logado.");
+    }
+
     $id = $_REQUEST['id'];
 
     $sql = "SELECT * FROM posts WHERE id = $id";   
@@ -13,9 +18,7 @@
 
 ?>
 
-<h3>Alterar Post</h3>
-
-<div class="form_edita_post">
+<div class="form_post">
     <h1>Editar Post</h1>
 
     <form action="edita_post.php" method="post">
@@ -24,8 +27,7 @@
             <input type="text"     name="titulo"     id="titulo"     value="<?=$posts['titulo'];?>"     required><br>
             <input type="textarea" name="comentario" id="comentario" value="<?=$posts['comentario'];?>" required><br>
             <input type="number"   name="avaliacao"  id="avaliacao"  value="<?=$posts['avaliacao'];?>"  required min="0" max="10" step="0.1" ><br>
-            <input type="hidden"   name="usuario_id" id="usuario_id" value="<?=$posts['usuario_id'];?>" readonly><br>
-            <input type="submit"   value="Postar">  
+            <input type="submit"   value="Editar">  
 
     </form>
 </div> 
