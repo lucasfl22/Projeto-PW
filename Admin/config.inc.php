e0cd25c2b4d34176fd1523a70023dcf41615d28c
Felipe Lorhans <felipe_mae@hotmail.com>
2024-11-20 16:11:50 -0300

    Update config.inc.php
    Atualizado com o nome correto do banco de dados.

--- Admin/config.inc.php
+++ Admin/config.inc.php
@@ -1,8 +1,8 @@
 <?php 
 // Conexão com servidor MySQL
 $conn = mysqli_connect("127.0.0.1","root","");
-// Conexão com o banco MySQL chamado Projeto-PW
-$db = mysqli_select_db($conn,"Projeto-PW");
+// Conexão com o banco MySQL chamado bd_biblioteca
+$db = mysqli_select_db($conn,"bd_biblioteca");
 
 if($conn){
     echo "Conexão estabelecida com sucesso";
