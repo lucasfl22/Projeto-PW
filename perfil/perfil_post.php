a8dc65f9678547fdeded600c9fab19e295936308
Lucas Leandro <lucasfariasleandro@gmail.com>
2024-11-27 07:17:59 -0300

    Corrigindo erro do botão no perfil do usuario

--- perfil/perfil_post.php
+++ perfil/perfil_post.php
@@ -40,21 +40,33 @@ if (isset($_GET['id'])) {
         <div class="atividades-perfil">
             <h2>Publicações Recentes</h2>
             <ul>
-                <?php
-                // Consulta para buscar as publicações do usuário
-                $sql_publicacoes = "SELECT titulo, comentario, avaliacao FROM posts WHERE usuario_id = $usuario_id";
+            <?php
+                // Consulta para buscar as publicações do usuário, incluindo o tipo
+                $usuario_id = $_SESSION['id_usuario'];
+                $sql_publicacoes = "SELECT titulo, comentario, avaliacao, tipo FROM posts WHERE usuario_id = $usuario_id";
                 $query_publicacoes = mysqli_query($conexao, $sql_publicacoes);
 
                 // Verifica se há publicações e exibe
                 if (mysqli_num_rows($query_publicacoes) > 0) {
                     while ($publicacao = mysqli_fetch_array($query_publicacoes)) {
+                        // Define a URL com base no tipo da publicação
+                        $url_publicacao = '';
+                        if ($publicacao['tipo'] === 'filme') {
+                            $url_publicacao = 'http://localhost/projetopw/index.php?pg=bate-papo/bate-papo_filme';
+                        } elseif ($publicacao['tipo'] === 'serie') {
+                            $url_publicacao = 'http://localhost/projetopw/index.php?pg=bate-papo/bate-papo_serie'; // Exemplo para série
+                        } elseif ($publicacao['tipo'] === 'livro') {
+                            $url_publicacao = 'http://localhost/projetopw/index.php?pg=bate-papo/bate-papo_livro'; // Exemplo para livro
+                        }
+
                         echo "<li><strong>Título:</strong> {$publicacao['titulo']}<br>";
+                        echo "<strong>Tipo:</strong> {$publicacao['tipo']}<br>";
                         echo "<strong>Comentário:</strong> {$publicacao['comentario']}<br>";
                         echo "<strong>Avaliação:</strong> {$publicacao['avaliacao']}<br>";
-                        echo "<hr></li>";
+                        echo "<a href='{$url_publicacao}' class='btn-publicacao'>Veja a Publicação</a><br><hr>";
                     }
                 } else {
-                    echo "<li>Nenhuma publicação encontrada.</li>";
+                    echo "<li>Você ainda não fez nenhuma publicação.</li>";
                 }
 
                 mysqli_close($conexao);
