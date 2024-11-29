2a112712a19b18908ccdc1f4aa8b7e26b33bc69b
Lucas Leandro <lucasfariasleandro@gmail.com>
2024-11-29 13:22:51 -0300

    adicao de botao de admin no menu para usuarios com acesso

--- components/menu/menu.php
+++ components/menu/menu.php
@@ -5,10 +5,16 @@
         <li><a href="?pg=bate-papo/bate-papo_serie">Veja Séries</a></li>
         <li><a href="?pg=bate-papo/bate-papo_livro">Veja Livros</a></li>
         <li><a href="#">Amizades</a></li>
-    <?php if (logado()){ ?>         
-        <li><a href="?pg=fale_conosco/form_envia_msg">Fale Conosco</button></a></li>
-    <?php } else{ ?>
-        <li><a href="?pg=acesso/login/form-login">Fale Conosco</button></a></li>
-    <?php } ?>
+
+        <?php if (logado()): ?> 
+            <li><a href="?pg=fale_conosco/form_envia_msg">Fale Conosco</a></li>
+        <?php else: ?>
+            <li><a href="?pg=acesso/login/form-login">Fale Conosco</a></li>
+        <?php endif; ?>
+
+        <!-- Verifica se 'acesso_admin' está definido e se é igual a 1 -->
+        <?php if (isset($_SESSION['acesso_admin']) && $_SESSION['acesso_admin'] == 1): ?>
+            <li><a href="admin/">Admin</a></li>
+        <?php endif; ?>
     </ul>
 </nav>
